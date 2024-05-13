dce21cdb5c8d5d0f739d52bc010453bf075f0f42
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-05-13 12:09:21 +0200

    In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all'utente il suo nome e comunicagli se può partecipare o no alla festa.

---
+++ SNACK-4/JS/script.js
@@ -0,0 +1,8 @@
+const nomeUtente = prompt ('Inserisci il tuo nome');
+const listaInvitati = ["Giuseppe", "Alessia","Gianmarco","Tony","Greta"]
+if(listaInvitati === nomeUtente){
+    console.log('Puoi entrare')
+}
+else{
+    console.log('Non puoi entrare')
+}
