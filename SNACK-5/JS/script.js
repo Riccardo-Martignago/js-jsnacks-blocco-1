15a578d2f404065ee49b8a9023b8117c76df91e5
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-05-13 12:35:07 +0200

    Crea un array vuoto.  Chiedi per 6 volte all'utente di inserire un numero,  se è dispari inseriscilo nell'array.

---
+++ SNACK-5/JS/script.js
@@ -0,0 +1,13 @@
+const array = []
+for(let i = 0; i < 5 ; i++){
+    const numeroUtente = parseInt (prompt ('Inserisci un  numero'), 10);
+
+    if(numeroUtente % 2 === 1){
+        array.push(numeroUtente)
+    }
+    else{
+        console.log('Non puo essere stampato nell array')
+    }
+}
+
+
