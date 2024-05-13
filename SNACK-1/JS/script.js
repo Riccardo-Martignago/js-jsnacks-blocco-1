c315a9129faa3d92e2f6c196fe5c92a8a327689f
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-05-13 11:50:34 +0200

    L'utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.

---
+++ SNACK-1/JS/script.js
@@ -0,0 +1,8 @@
+let request = prompt ('Inserisci un numero')
+let requestSecond = prompt ('Inserisci un numero')
+if(request > requestSecond){
+    console.log(request)
+}
+else{
+    console.log(requestSecond)
+}
